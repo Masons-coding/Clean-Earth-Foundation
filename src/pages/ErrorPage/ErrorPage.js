54afbb314dc58df372e8b113a1a66aae641f6cc2
Mason Clarke <maclarkegdci@gmail.com>
2023-01-18 17:03:18 -0500

    Updated 404 page
    Updated error page that user is taken to when they try to go to a page that does not exist.

--- src/pages/ErrorPage/ErrorPage.js
+++ src/pages/ErrorPage/ErrorPage.js
@@ -1,15 +1,16 @@
 import './ErrorPage.scss';
 
-import errorImage from "../../assets/images/404.webp";
 import Footer from "../../components/Footer/Footer.js";
+
+import errorIcon from "../../assets/images/404Page.png"
 
 
 const ErrorPage = () => {
     return (
         <>
-        <p className="error-page__message">Page does not exist!</p>
         <div className="error-page__container">
-            <img className="error-page__image" src={errorImage} alt="404 error"/>
+            <img className="error-page__image" src={errorIcon} alt="404 error"/>
+            <p className="error-page__message">SORRY, PAGE NOT FOUND!</p>
         </div>
         <Footer/>
         </>
