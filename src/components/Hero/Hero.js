a128c36f36893df7204ef020839d7ff7bc2b7abb
Mason Clarke <maclarkegdci@gmail.com>
2023-01-18 16:45:38 -0500

    Updated hero
    Updated hero to have buttons for leading and joining clean ups. Also made the join a clean up button hidden when the user is not logged in.

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,8 +4,42 @@ import { useNavigate  } from 'react-router-dom';
 
 import leadCleanUpIcon from "../../assets/images/icons/LeadCleanUpIcon.svg";
 import joinCleanUpIcon from "../../assets/images/icons/JoinCleanUpIcon.svg";
+
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const USER_CURRENT = process.env.REACT_APP_USER_CURRENT_URL;
+
+const API = process.env.REACT_APP_API_KEY;
+
+const urlForUserCurrent =`${USER_CURRENT}${API}`;
   
 const Hero = () => {
+
+  const [failedAuth, setFailedAuth] = useState(true);
+
+  const authToken = sessionStorage.getItem('authToken');
+
+  // if there is an error from the endpoint (ie: token invalid, expired, tampered with)
+  useEffect(() => {
+      if (!authToken){
+          setFailedAuth(true);
+      }else{
+          axios
+          .get((urlForUserCurrent), {
+            headers: {
+              Authorization: `Bearer ${authToken}`,
+            },
+          })
+          .then((res) => {
+            setFailedAuth(false);
+          })
+          .catch((err) => {
+            setFailedAuth(true);
+          })
+      }
+
+  }, [authToken]);
 
   const navigateVolunteerPage = useNavigate();
 
@@ -17,20 +51,20 @@ const Hero = () => {
   const handleJoinClick = () => {
     window.scrollTo(0, 900);
 };
-  
+
   return (
     <section className="hero">
       <div className="hero__text-button-container">
         <h1 className="hero__title">WELCOME TO CLEAN EARTH</h1>
         <p className="hero__text">Register to become a volunteer with Clean Earth and we will notify you if there are cleanup events going on in your region or help you run your own event!</p>
           <div className="hero__lead-join-container">
-            <div className="hero__lead-container">
+            <div onClick={handleLeadClick} className="hero__lead-container">
               <img src={leadCleanUpIcon} alt="Map and pencil"/>
-              <button onClick={handleLeadClick} className="hero__button-lead">Lead a clean up!</button>
+              <button className="hero__button-lead">Lead a clean up!</button>
             </div>
-            <div className="hero__join-container">
+            <div onClick={handleJoinClick} className={failedAuth === true ? "hero__join-container-hidden" : "hero__join-container" }>
               <img src={joinCleanUpIcon} alt="Map with place marker"/>
-              <button onClick={handleJoinClick} className="hero__button-join">Join a clean up!</button>
+              <button className="hero__button-join" >Join a clean up!</button>
             </div>
           </div>
         </div>
